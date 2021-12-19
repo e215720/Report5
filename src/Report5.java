f0095f8ad58fd03e9f3f77b1ff6cd5f271de8faf
kazuki kinjo <e215720@ie.u-ryukyu.ac.jp>
2021-12-19 16:47:57 +0900

    step3

--- src/Report5.java
+++ src/Report5.java
@@ -1,8 +1,9 @@
 public class Report5{
     public static void main(String[] args){
-        String str = null;
+        String str = "百二十三";
         try{
-            System.out.println(str.length());
+            int value = Integer.parseInt(str);
+            System.out.println(value);
         }catch(NullPointerException e){
             System.out.println("NullPointerExceptionが発生");
             System.out.println(e.getMessage());
